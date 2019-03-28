809c5cc3d6b0f9efdad0318b39a04bb6396c50d8
Scott Prahl <scott@prahls.com>
2019-03-28 12:34:06 -0700

    new version number

--- src/ad_layers.h
+++ src/ad_layers.h
@@ -1,41 +1,24 @@
-/*2:*/
-#line 19 "ad_layers.w"
 
 
-#line 20 "ad_layers.w"
 
-/*14:*/
-#line 229 "ad_layers.w"
 
-void RT_Layers(int n,
-double nslab,
-double ntopslide,
-double nbottomslide,
-int nlayers,
-double a[],
-double b[],
-double g[],
-double*UR1,double*UT1,double*URU,double*UTU)
+void RT_Layers (int n,
+		double nslab,
+		double ntopslide,
+		double nbottomslide,
+		int nlayers,
+		double a[],
+		double b[],
+		double g[],
+		double *UR1, double *UT1, double *URU, double *UTU);
 
-/*:14*/
-#line 21 "ad_layers.w"
-;
-/*3:*/
-#line 36 "ad_layers.w"
-
-void RT_Layers_All(int n,
-double nslab,
-double ntopslide,
-double nbottomslide,
-int nlayers,
-double a[],
-double b[],
-double g[],
-double*dUR1,double*dUT1,double*dURU,double*dUTU,
-double*uUR1,double*uUT1,double*uURU,double*uUTU)
-
-/*:3*/
-#line 22 "ad_layers.w"
-;
-
-/*:2*/
+void RT_Layers_All (int n,
+		    double nslab,
+		    double ntopslide,
+		    double nbottomslide,
+		    int nlayers,
+		    double a[],
+		    double b[],
+		    double g[],
+		    double *dUR1, double *dUT1, double *dURU, double *dUTU,
+		    double *uUR1, double *uUT1, double *uURU, double *uUTU);
