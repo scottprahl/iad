7012e64b907ab9c1c1c1014c409c68cc89ea95e9
Work Prahl <prahl@bme.ogi.edu>
2019-06-24 12:25:29 -0700

    adding libiad.h to avoid ctangle issues

--- src/version.c
+++ src/version.c
@@ -2,4 +2,4 @@
          The version number obtained from       <../Makefile>
          The date is the last change to         <version.h>
 */
-char *Version = "3-11-0 (5 Mar 2019)";
+char *Version = "3-11-0 (2 Nov 2017)";
